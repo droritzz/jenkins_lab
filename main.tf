489cd86fbf32ef51df8fdb4dd1d834b609f14dbd
droritzz <droritzz@gmail.com>
2021-09-20 10:49:40 +0300

    creating test branch for terrform

--- main.tf
+++ main.tf
@@ -42,62 +42,77 @@ resource "aws_network_interface" "slave_ip" {
   }
 }
 
-#resource "aws_security_group" "srv_security_group" {
-#  name   = "srv_security_group"
-#  vpc_id = aws_vpc.srv_vpc.id
-#  ingress = [
-#    {
-#      from_port   = 22
-#      to_port     = 22
-#      protocol    = "tcp"
-#      cidr_blocks = "172.31.0.0/24"
-#    }
-#  ]
-#  egress = [
-#    {
-#      from_port   = 0
-#      to_port     = 0
-#      cidr_blocks = ["0.0.0.0/0"]
-#    }
-#  ]
-#  tags = {
-#    Name = "srv_security_group"
-#  }
-#}
+resource "aws_security_group" "srv_security_group" {
+  name   = "srv_security_group"
+  vpc_id = aws_vpc.srv_vpc.id
 
-#resource "aws_eip" "external_ip_master" {
-#  instance                  = "aws_instance.master"
-#  associate_with_private_ip = aws_network_interface.master_ip.id
-#  vpc                       = true
-#  depends_on = [aws_internet_gateway.gateway]
-#}
+
+
+  ingress = [
+    {
+      description      = "security group inbound rule for SSH"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "SSH"
+      cidr_blocks = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [aws_vpc.srv_vpc.ipv6_cidr_block]
+    },
+    {
+      description      = "security group inbound rule for Jenkins"
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [aws_vpc.srv_vpc.ipv6_cidr_block]
+    }
+  ]
+  egress = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
+  tags = {
+    Name = "srv_security_group"
+  }
+}
+
+
 
 resource "aws_instance" "master" {
   count = 1
   ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
 
+  associate_public_ip_address = True
+
   network_interface {
     network_interface_id = aws_network_interface.master_ip.id
     device_index         = 0
   }
+  
+  vpc_security_group_ids = [aws_security_group.srv_security_group.id]
 
   tags = {
     Name = "jenkins master "
   }
 }
 
-#resource "aws_eip" "external_ip_slave" {
-#  instance                  = "aws_instance.slave"
-#  associate_with_private_ip = aws_network_interface.slave_ip.id
-#  vpc                       = true
-# depends_on = [aws_internet_gateway.gateway]
-#}
+
+resource "aws_network_interface" "multi-ip" {
+  subnet_id   = aws_subnet.srv_subnet.id
+  private_ips = ["10.0.0.10", "10.0.0.11"]
+}
 
 resource "aws_instance" "slave" {
   count = 1
   ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
+  associate_public_ip_address = True
 
   network_interface {
     network_interface_id = aws_network_interface.slave_ip.id
